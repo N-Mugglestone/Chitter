c0b0338ecb17c449159b3aa9243705b56b476f57
Naomi Mugglestone <NJLM2708@outlook.com>
2022-03-22 16:08:05 +0000

    formatting

--- frontEnd/src/Components/utils/footer.jsx
+++ frontEnd/src/Components/utils/footer.jsx
@@ -1,10 +1,12 @@
+import '../CSS/footer.css'
+
 const Footer = () => {
     return (
         <>
             <div className="footer">
                 <footer class="py-3 fixed-bottom">
                     <div class="m-0 text-center text-white">
-                        copyright &copy; Chitter 2022
+                        <h3>Copyright &copy; Chitter 2022</h3>
                     </div>
                 </footer>
             </div>
--- frontEnd/src/Components/utils/header.jsx
+++ frontEnd/src/Components/utils/header.jsx
@@ -1,24 +1,23 @@
-
+import '../CSS/header.css'
 
 const Header = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#"> &nbsp; </a>
-                    <a class="nav-link disabled"> Sign-up </a>
-                    <button class="navbar-toggler" type="button" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarAltMArkup" aria-expanded="false" aria-label="Toggle navigation" >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link" href='#'>Profile</a>
-                            <a class="nav-link" href='#'>Edit</a>
+            <title> Chitter </title>
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link" href="#">Home</a>
+                                <a class="nav-link" href='#'>Register</a>
+                                <a class="nav-link" href='#'>Login</a>
+                                <a class="nav-link" href='#'>Logout</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-            </nav>
+                </nav>
+            </header>
         </>
     )
 }
